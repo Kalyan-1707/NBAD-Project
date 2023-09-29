5fe1de318ad2e354576c3ebfcaf8a41c541f1644
Pavan Kalyan Konudula <52063829+Kalyan-1707@users.noreply.github.com>
2023-09-29 15:00:57 -0400

    migration to ejs
    Initialized express app
    migrated all html files to ejs
    added partials
    configured connections for dynamic routing

---
+++ app.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// enabling styles/images to be served from public
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('index')
+});
+
+
+app.get('/newConnection', (req, res) => {
+    res.render('newConnection')
+});
+
+app.get('/connections', (req, res) => {
+    fs.readFile("./events.json", "utf8", (err, data) => {
+        if(err){
+            console.log(err);
+            res.statusCode(500);
+            
+        }else{
+            const listOfConnections = JSON.parse(data);      
+            res.render('connections',{listOfConnections});
+        }
+    })
+})
+
+app.get('/connection/:id', (req, res) => {
+    const id = req.params.id;
+
+    fs.readFile("./events.json", "utf8", (err, data) => {
+        if(err){
+            console.log(err);
+            res.statusCode(500);
+            
+        }else{
+            const listOfConnections = JSON.parse(data);      
+            let currConnection;
+            let found = false;
+            for(const category in listOfConnections){
+                if(found === true){
+                    break;
+                }
+                for(const connection of listOfConnections[category]){
+                    if(connection.id === id){
+                        currConnection = connection;
+                        found = true;
+                        break;
+                    }
+                }
+            }
+            res.render('connection',{connection:currConnection});
+        }
+    })
+})
+
+app.listen(3000,() => {
+    console.log('Listening on port 3000');
+
+});
